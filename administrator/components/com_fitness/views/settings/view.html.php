3f473159e7df6af8af4a7660d7d0bfa1e8aad13f
nick <nick@nick-desktop>
2013-06-18 19:32:42 +0300

    locations view ready2

--- administrator/components/com_fitness/views/settings/view.html.php
+++ administrator/components/com_fitness/views/settings/view.html.php
@@ -43,6 +43,7 @@ class FitnessViewSettings extends JView {
         $view = $input->getCmd('view', '');
         FitnessHelper::addSubmenu('Dashboard', 'dashboard');
         FitnessHelper::addSubmenu('Categories', 'categories');
+        FitnessHelper::addSubmenu('Locations', 'locations');
 
 
         parent::display($tpl);
