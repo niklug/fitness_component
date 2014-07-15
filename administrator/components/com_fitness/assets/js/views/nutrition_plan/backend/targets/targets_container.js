27dc3985db1cb0a6e5ccfdd2bce35bc9ae90c80e
nick <nick@nick-Presario-CQ57-Notebook-PC>
2014-07-15 17:58:43 +0300

    Nutrition Plan  TARGETS - details adjusted

--- administrator/components/com_fitness/assets/js/views/nutrition_plan/backend/targets/targets_container.js
+++ administrator/components/com_fitness/assets/js/views/nutrition_plan/backend/targets/targets_container.js
@@ -31,12 +31,30 @@ define([
             var self = this;
             $(this.el).show('0', function() {
                 self.loadStep1();
+                self.connectComments();
             });
         },
         
         loadStep1 : function() {
             app.views.targets_step1 = new Step1_view({model : this.model});
             $(this.el).find("#step1_wrapper").html(app.views.targets_step1.render().el);
+        },
+        
+        connectComments : function() {
+            // connect comments
+             var comment_options = {
+                'item_id' :  this.options.item_model.get('id'),
+                'fitness_administration_url' : app.options.ajax_call_url,
+                'comment_obj' : {'user_name' : app.options.user_name, 'created' : "", 'comment' : ""},
+                'db_table' :  '#__fitness_nutrition_plan_targets_comments',
+                'read_only' : false,
+                'anable_comment_email' : true,
+                'comment_method' : 'TargetsComment'
+            }
+            var comments =  $.comments(comment_options, comment_options.item_id, 0);
+
+            var comments_html = comments.run();
+            $(this.el).find("#targets_comments_wrapper").html(comments_html);
         }
 
    
